df67935a4a69ca1fa1df79aa2421d9c45418c95e
Adrian PK <adrianpk@adrianpk.com>
2025-07-06 14:23:29 +0200

    Add migration and basic queries for section

--- assets/query/sqlite/ssg/content.sql
+++ assets/query/sqlite/ssg/content.sql
@@ -3,9 +3,9 @@
 
 -- Create
 INSERT INTO content (
-    id, user_id, heading, body, status, short_id, created_by, updated_by, created_at, updated_at
+    id, short_id, user_id, heading, body, status, created_by, updated_by, created_at, updated_at
 ) VALUES (
-    :id, :user_id, :heading, :body, :status, :short_id, :created_by, :updated_by, :created_at, :updated_at
+    :id, :short_id, :user_id, :heading, :body, :status, :created_by, :updated_by, :created_at, :updated_at
 );
 
 -- GetAll
