f1b75835e03256965f1e4d418030bd62b9165cfb
Adrian PK <adrianpk@adrianpk.com>
2025-07-06 13:56:58 +0200

    Add image fields to section

--- internal/feat/ssg/sectionda.go
+++ internal/feat/ssg/sectionda.go
@@ -8,11 +8,13 @@ import (
 
 type SectionDA struct {
 	ID          uuid.UUID      `db:"id"`
+	ShortID     sql.NullString `db:"short_id"`
 	Name        sql.NullString `db:"name"`
 	Description sql.NullString `db:"description"`
 	Path        sql.NullString `db:"path"`
-	LayoutID    sql.NullString    `db:"layout_id"`
-	ShortID     sql.NullString `db:"short_id"`
+	LayoutID    sql.NullString `db:"layout_id"`
+	Image       sql.NullString `db:"image"`
+	Header      sql.NullString `db:"header"`
 	CreatedBy   sql.NullString `db:"created_by"`
 	UpdatedBy   sql.NullString `db:"updated_by"`
 	CreatedAt   sql.NullTime   `db:"created_at"`
